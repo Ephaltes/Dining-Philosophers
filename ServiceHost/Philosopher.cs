341678f148648d36b162e1bb93538aa84ee78c63
Ephaltes <ngoc.lam@live.at>
2023-02-23 18:49:10 +0100

    zwischenstand

--- ServiceHost/Philosopher.cs
+++ ServiceHost/Philosopher.cs
@@ -2,23 +2,55 @@
 
 public class Philosopher
 {
-    public string Name { get; set; }
-    public Fork LeftFork { get; set; }
-    public Fork RightFork { get; set; }
-    private int _thinkingTime;
-    private int _eatingTime;
+    private readonly string _name;
+    private readonly Fork _leftFork;
+    private readonly Fork _rightFork;
+    private readonly int _thinkingTime;
+    private readonly int _eatingTime;
+    private readonly Random _random;
 
     public Philosopher(string name, Fork leftFork, Fork rightFork, int thinkingTime, int eatingTime)
     {
-        Name = name;
-        LeftFork = leftFork;
-        RightFork = rightFork;
+        _name = name;
+        _leftFork = leftFork;
+        _rightFork = rightFork;
         _thinkingTime = thinkingTime;
         _eatingTime = eatingTime;
+        _random = new Random(Guid.NewGuid().GetHashCode());
     }
 
     public void Eat()
     {
+        int thinkingTime = _random.Next(_thinkingTime);
+        int eatingTime = _random.Next(_eatingTime);
         
+        Thread.Sleep(thinkingTime);
+        Console.WriteLine($"Name: {_name} finished Thinking");
+        
+        Console.WriteLine($"Name: {_name} takes Left-Fork: {_leftFork.Name}");
+        TakeLeftFork();
+        Console.WriteLine($"Name: {_name} takes Right-Fork: {_rightFork.Name}");
+        TakeRightFork();
+
+        Thread.Sleep(eatingTime);
+        Console.WriteLine($"Name: {_name} finished eating");
+
+        PutForksBack();
+    }
+
+    private void PutForksBack()
+    {
+        Monitor.Exit(_leftFork);
+        Monitor.Exit(_rightFork);
+    }
+
+    private void TakeRightFork()
+    {
+        Monitor.Enter(_rightFork);
+    }
+
+    private void TakeLeftFork()
+    {
+        Monitor.Enter(_leftFork);
     }
 }
--- ServiceHost/Program.cs
+++ ServiceHost/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ServiceHost;
+
+Console.WriteLine("Enter number of Philosophers dinning: ");
+int philosopherCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter max thinking Time: ");
+int maxThinkingTime = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter max eating Time: ");
+int maxEatingTime = Convert.ToInt32(Console.ReadLine());
+
+List<Fork> forks =  new List<Fork>();
+List<Philosopher> philosophers = new List<Philosopher>();
+
+for (int i = 0; i < philosopherCount; i++)
+{
+    forks.Add(new Fork($"Fork_{i}"));
+}
+
+for (int i = 0; i < philosopherCount; i++)
+{
+    philosophers.Add(new Philosopher(
+        $"Philosopher_{i}",
+        forks[i],
+        forks[(i+1) % philosopherCount],
+        maxThinkingTime,
+        maxEatingTime
+    ));
+}
+
+foreach (Philosopher philosopher in philosophers)
+{
+    philosopher.Eat();
+}
