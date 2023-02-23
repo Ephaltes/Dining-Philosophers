b8e15dabeaf465bdb30f2901a5b658a8f3d2a82a
Ephaltes <ngoc.lam@live.at>
2023-02-23 16:45:40 +0100

    init

---
+++ ServiceHost/Fork.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceHost;
+
+public class Fork
+{
+    public Fork(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
---
+++ ServiceHost/Philosopher.cs
@@ -0,0 +1,24 @@
+﻿namespace ServiceHost;
+
+public class Philosopher
+{
+    public string Name { get; set; }
+    public Fork LeftFork { get; set; }
+    public Fork RightFork { get; set; }
+    private int _thinkingTime;
+    private int _eatingTime;
+
+    public Philosopher(string name, Fork leftFork, Fork rightFork, int thinkingTime, int eatingTime)
+    {
+        Name = name;
+        LeftFork = leftFork;
+        RightFork = rightFork;
+        _thinkingTime = thinkingTime;
+        _eatingTime = eatingTime;
+    }
+
+    public void Eat()
+    {
+        
+    }
+}
